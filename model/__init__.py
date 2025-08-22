57b82420257dfaf18e95fc2bd868441455f3ba2c
90828 <908288050@qq.com>
2025-08-22 19:24:57 +0800

    改卷积

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,10 @@
 from .drconv import *
+from .basic import *
 from .layernorm import *
 from .swin import *
 from .mdfusion import *
 from .backbone_ublock import *
 from .loss import *
-from .basic import *
+
 # from .layers import *
 # from .backbone_convir import *
--- model/mdfusion.py
+++ model/mdfusion.py
@@ -7,38 +7,38 @@ import torch.nn.functional as F
 class MDFusion(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.proj = nn.Conv2d(in_channels,in_channels,kernel_size=3)
+        self.proj = nn.Conv2d(in_channels,in_channels,kernel_size=3,padding=1)
         self.d0 = nn.Sequential(
-            BasicDrConv(in_channels,in_channels/2),
-            BasicConv(in_channels/2,in_channels),
+            BasicDrConv(in_channels,in_channels//2),
+            BasicConv(in_channels//2,in_channels),
             BasicDrConv(in_channels,in_channels,kernel_length=5)
         )
         self.d1 = nn.Sequential(
-            BasicDrConv(in_channels,in_channels/2,direction=1),
-            BasicConv(in_channels/2,in_channels),
+            BasicDrConv(in_channels,in_channels//2,direction=1),
+            BasicConv(in_channels//2,in_channels),
             BasicDrConv(in_channels,in_channels,direction=1,kernel_length=5)
         )
         self.d2 = nn.Sequential(
-            BasicDrConv(in_channels,in_channels/2,direction=2),
-            BasicConv(in_channels/2,in_channels),
+            BasicDrConv(in_channels,in_channels//2,direction=2),
+            BasicConv(in_channels//2,in_channels),
             BasicDrConv(in_channels,in_channels,direction=2,kernel_length=5)
         )
         self.d3 = nn.Sequential(
-            BasicDrConv(in_channels,in_channels/2,direction=3),
-            BasicConv(in_channels/2,in_channels),
+            BasicDrConv(in_channels,in_channels//2,direction=3),
+            BasicConv(in_channels//2,in_channels),
             BasicDrConv(in_channels,in_channels,direction=3,kernel_length=5)
         )
         self.mixer = nn.Conv2d(in_channels,out_channels,kernel_size=1)
         self.attn = nn.Sequential(
             SwinBlock(dim=in_channels, input_resolution=None, num_heads=4, shift_size=0),
             SwinBlock(dim=in_channels, input_resolution=None, num_heads=4, shift_size=7),
-            nn.Conv2d(in_channels, in_channels, kernel_size=1)
+            nn.Conv2d(in_channels, out_channels, kernel_size=1)
         )
 
 
     def forward(self, x):
         inp = self.proj(x)
-        res = self.d1(inp)+self.d2(inp)+self.d3(inp)+self.d4(inp)
+        res = self.d0(inp)+self.d1(inp)+self.d2(inp)+self.d3(inp)
         res = self.mixer(res)
         out = inp*res
         return x+self.attn(out)
